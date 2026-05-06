48aee61b91bb777f3f747d8c7fb9cf470094c8a3
FelipeTinel <felipemacedotinel@gmail.com>
2026-05-06 10:21:11 -0300

    feat: add order logic

--- protoype/src/main/java/model/Costumer.java
+++ protoype/src/main/java/model/Costumer.java
@@ -69,6 +69,11 @@ public class Costumer{
         return cpf;
     }
 
+    public boolean makePayment (double value) {
+
+        
+
+    }
 
 
 }
--- protoype/src/main/java/model/Order.java
+++ protoype/src/main/java/model/Order.java
@@ -1,20 +1,26 @@
 package model;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
 
     private int id;
-    private List<Item> order;
-    private double totalPrice;
+    private List<Item> order = new ArrayList<>();
+    private double totalPrice = 0;
 
     Order () {}
-
+    
     Order (int id, List<Item> order, double totalPrice) {
-
+        
         setId(id);
         setOrder(order);
-        setTotalPrice(totalPrice);
-
+        
+    }
+    
+    private void calculateTotalPrice() {
+        for (Item item : order) {
+            totalPrice += item.getPrice();
+        }
     }
 
     public void setId(int id) {
@@ -22,6 +28,7 @@ public class Order {
     }
 
     public void setOrder(List<Item> order) {
+        calculateTotalPrice();
         this.order = order;
     }
 
@@ -40,4 +47,16 @@ public class Order {
     public double getTotalPrice() {
         return totalPrice;
     }
+
+    public void addItem(Item item) {
+        order.add(item);
+        calculateTotalPrice();
+    }
+
+    public void deleteItem(Item item) {
+        order.remove(item);
+        calculateTotalPrice();
+    }
+
+
 }
